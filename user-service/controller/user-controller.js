c99ac07a910ccc98275d9f226c2319e7641ed85a
Wu HanHui <wuhanhui7@gmail.com>
2022-08-28 05:03:00 +0800

    Add check duplicate user logic and hash password

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -1,21 +1,53 @@
-import { ormCreateUser as _createUser } from '../model/user-orm.js'
+import { ormCreateUser, ormLoginUser, ormFindUser } from '../model/user-orm.js'
+
+const CREATION_ERROR = 'Could not create a new user!';
+const DATABASE_ERROR = 'Database failure when creating new user!';
+const MISSING_USERNAME_OR_PASSWORD_ERROR = 'Username and/or Password are missing!';
+const USERNAME_TAKEN_ERROR = 'Username already taken!';
+const WRONG_USERNAME_OR_PASSWORD_ERROR = 'Username and/or Password are wrong!';
+
+const CREATE_SUCCESS_MESSAGE = 'New user created successfully!';
+const LOGIN_SUCCESS_MESSAGE = 'Login successfully!'
 
 export async function createUser(req, res) {
     try {
         const { username, password } = req.body;
-        if (username && password) {
-            const resp = await _createUser(username, password);
-            console.log(resp);
-            if (resp.err) {
-                return res.status(400).json({message: 'Could not create a new user!'});
-            } else {
-                console.log(`Created new user ${username} successfully!`)
-                return res.status(201).json({message: `Created new user ${username} successfully!`});
-            }
+        if (!username || !password) {
+            return res.status(400).json({message: MISSING_USERNAME_OR_PASSWORD_ERROR});
+        }
+        const user = await ormFindUser(username);
+        if (user !== null) {
+            return res.status(409).json({message: USERNAME_TAKEN_ERROR})
+        }
+
+        const resp = await ormCreateUser(username, password);
+        if (resp.err) {
+            return res.status(400).json({message: CREATION_ERROR});
         } else {
-            return res.status(400).json({message: 'Username and/or Password are missing!'});
+            return res.status(201).json({message: CREATE_SUCCESS_MESSAGE});
         }
     } catch (err) {
-        return res.status(500).json({message: 'Database failure when creating new user!'})
+        console.log(err)
+        return res.status(500).json({message: DATABASE_ERROR})
+    }
+}
+
+export async function loginUser(req, res) {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({message: MISSING_USERNAME_OR_PASSWORD_ERROR});
+        }
+        const user = await ormFindUser(username);
+        if (user === null) {
+            return res.status(400).json({message: WRONG_USERNAME_OR_PASSWORD_ERROR})
+        }
+        const login = await ormLoginUser(user, password);
+        if (!login) {
+            return res.status(400).json({message: WRONG_USERNAME_OR_PASSWORD_ERROR});
+        }
+        return res.status(200).json({message: LOGIN_SUCCESS_MESSAGE});
+    } catch (err) {
+        return res.status(500).json({message: DATABASE_ERROR})
     }
 }
