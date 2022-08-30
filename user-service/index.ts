776386b480167ba1fd221e8d562da912f6c904e3
Wu HanHui <wuhanhui7@gmail.com>
2022-08-31 05:32:35 +0800

    Add typescript to user service

---
+++ user-service/index.ts
@@ -0,0 +1,25 @@
+import cors from "cors";
+import express from "express";
+import { createUser, loginUser } from "./controller/user-controller";
+import { body } from "express-validator";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors()); // config cors so that front-end can use
+//app.options('*', cors())
+
+const router = express.Router();
+
+// Controller will contain all the User-defined Routes
+router.get("/", (_, res) => res.send("Hello World from user-service"));
+router.post("/", body("password").isLength({ min: 5 }), createUser);
+router.post("/login", loginUser);
+
+app.use("/api/user", router);
+app.all("*", (_, res) => {
+  res.setHeader("content-type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+});
+
+app.listen(8000, () => console.log("user-service listening on port 8000"));
---
+++ user-service/model/repository.ts
@@ -0,0 +1,27 @@
+import UserModel from "./user-model";
+import "dotenv/config";
+
+//Set up mongoose connection
+import mongoose from "mongoose";
+
+const mongoDB =
+  process.env.ENV == "PROD"
+    ? process.env.DB_CLOUD_URI
+    : process.env.DB_LOCAL_URI;
+
+mongoose.connect(`${mongoDB}`, { autoIndex: false });
+
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+export function createUser(username: string, pHash: string) {
+  return new UserModel({
+    username: username,
+    pHash: pHash,
+  });
+}
+
+export async function findUser(username: string) {
+  const user = await UserModel.findOne({ username: username }).exec();
+  return user;
+}
