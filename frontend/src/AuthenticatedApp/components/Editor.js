e8ca55fd90189e71b4ca1829cf7bca2b11a5e168
Wu HanHui <wuhanhui7@gmail.com>
2022-10-23 05:32:04 +0800

    Change to use websocket for collab

--- frontend/src/AuthenticatedApp/components/Editor.js
+++ frontend/src/AuthenticatedApp/components/Editor.js
@@ -3,52 +3,30 @@ import CodeMirror from '@uiw/react-codemirror';
 import RandomColor from "randomcolor";
 import { dracula } from '@uiw/codemirror-theme-dracula';
 import { javascript } from '@codemirror/lang-javascript';
-import { useEffect, useState } from 'react';
-import { WebrtcProvider } from 'y-webrtc'
+import { WebsocketProvider } from 'y-websocket'
 import { yCollab } from "y-codemirror.next";
 import { useAuthContext } from '../../hooks/auth/useAuthContext';
 import "../../styles/editor.css"
 
 const Editor = (props) => {
-  const { room, password, height, ydoc, yText } = props;
-  const [collab, setCollab] = useState(null);
+  const { room, height, ydoc, yText } = props;
   const { userDetails } = useAuthContext();
 
-  useEffect(() => {
-    //syncs the ydoc throught WebRTC connection
-    const provider = new WebrtcProvider(
-      room,
-      ydoc,
-      {
-        signaling: [
-          "wss://signaling.yjs.dev",
-          'wss://y-webrtc-signaling-eu.herokuapp.com',
-        ],
-        password: password,
-      }
-    );
+  const provider = new WebsocketProvider(
+    'wss://demos.yjs.dev',
+    room,
+    ydoc,
+  );
 
-    // Undo manager used for stacking the undo and redo operation for yjs
-    const yUndoManager = new Y.UndoManager(yText);
-    const awareness = provider.awareness;
-    const color = RandomColor();
-    // Awareness protocol is used to propagate your information (cursor position , name , etc)
-    awareness.setLocalStateField("user", {
-      name: userDetails.username,
-      color: color,
-    });
-    setCollab(yCollab(yText, provider.awareness, { yUndoManager }));
-    return () => {
-      if (provider) {
-        provider.disconnect();
-        provider.destroy();
-      }
-    };
-  }, [ydoc, yText, room, password]);
-
-  if (!collab) {
-    return null;
-  }
+  // Undo manager used for stacking the undo and redo operation for yjs
+  const yUndoManager = new Y.UndoManager(yText);
+  const awareness = provider.awareness;
+  const color = RandomColor();
+  // Awareness protocol is used to propagate your information (cursor position , name , etc)
+  awareness.setLocalStateField("user", {
+    name: userDetails.username,
+    color: color,
+  });
 
   return (
     <CodeMirror
@@ -58,7 +36,7 @@ const Editor = (props) => {
       theme={dracula}
       extensions={[
         javascript({ jsx: true }),
-        collab
+        yCollab(yText, provider.awareness, { yUndoManager })
       ]}
     />
   );
