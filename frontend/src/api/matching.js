94c4e28936837b769e851e56ee8e14fc09097343
Kwek Min Yih <minyihkwek@gmail.com>
2022-10-26 20:01:47 +0800

    Rename environment variables

--- frontend/src/api/matching.js
+++ frontend/src/api/matching.js
@@ -1,6 +1,7 @@
 import io from 'socket.io-client';
 
-const SOCKET_URL = "http://localhost:8001/"
+const SOCKET_URL = process.env.REACT_APP_ENV_SOCKET_URL || "http://localhost:8001/"
+
 const socket = io(SOCKET_URL, {
   withCredentials: true,
   path: "/api/matching",
