f5f6cf8ef91f759b155b5e64a98d782e561a0b6a
Wu HanHui <wuhanhui7@gmail.com>
2022-10-28 12:59:11 +0800

    Add collab server

---
+++ collab-server/server.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+/**
+ * @type {any}
+ */
+const WebSocket = require('ws')
+const http = require('http')
+const wss = new WebSocket.Server({ noServer: true })
+const setupWSConnection = require('./utils.js').setupWSConnection
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end('okay')
+})
+
+wss.on('connection', setupWSConnection)
+
+server.on('upgrade', (request, socket, head) => {
+  // You may check auth of request here..
+  // See https://github.com/websockets/ws#client-authentication
+  /**
+   * @param {any} ws
+   */
+  const handleAuth = ws => {
+    wss.emit('connection', ws, request)
+  }
+  wss.handleUpgrade(request, socket, head, handleAuth)
+})
+
+server.listen(8004, () => {
+  console.log(`running on port ${8004}`)
+})
