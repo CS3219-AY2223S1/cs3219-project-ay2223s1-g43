430e24424cd56f87da4378159cc43a287f6bd913
Wu HanHui <wuhanhui7@gmail.com>
2022-10-29 11:41:03 +0800

    Add test cases and set up CI

--- user-service/controller/users/createUser.ts
+++ user-service/controller/users/createUser.ts
@@ -5,7 +5,7 @@ import { validationResult } from "express-validator";
 export default async function createUser(req, res) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: error_msg.PASSWORD_ERROR });
+    return res.status(400).json({ message: error_msg.PASSWORD_ERROR });
   }
 
   try {
