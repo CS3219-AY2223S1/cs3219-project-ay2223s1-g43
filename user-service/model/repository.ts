2b54ac5e38104f812d8a565a345328145974fd84
Wu HanHui <wuhanhui7@gmail.com>
2022-10-30 11:09:12 +0800

    Add learning pathway test cases

--- user-service/model/repository.ts
+++ user-service/model/repository.ts
@@ -6,7 +6,7 @@ import "dotenv/config";
 const mongoDB =
   process.env.ENV == "PROD"
     ? process.env.DB_CLOUD_URI
-    : process.env.DB_TEST_URI;
+    : 'mongodb://localhost:27017/test';
 
 mongoose.connect(`${mongoDB}`, { autoIndex: false });
 
